c47d1f804066d49feecb5bbaecb5cd718146e76b
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-11 18:07:58 -0400

    feat: make absurdly basic grid layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,12 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="flex min-h-screen items-center justify-center">
+	<div class="grid h-96 w-5xl grid-cols-[1fr_3fr] gap-4 rounded-lg">
+		<div class="rounded-lg border p-4">stuff</div>
+		<div class="rounded-lg border p-4">
+			{@render children()}
+		</div>
+	</div>
+</div>
