c3f38e14afb43039025e63f45bc04f3c60d10ff5
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-12 14:19:22 -0400

    feat: move stuff that should only be on home page out of `+layout.svelte`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,11 +25,4 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <Navbar />
-<div class="flex min-h-screen items-center justify-center">
-	<div class="grid h-96 w-5xl grid-cols-[1fr_3fr] gap-4 rounded-lg">
-		<div class="rounded-lg border p-4">stuff</div>
-		<div class="rounded-lg border p-4">
-			{@render children()}
-		</div>
-	</div>
-</div>
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex min-h-screen items-center justify-center">
+	<div class="grid h-96 w-5xl grid-cols-[1fr_3fr] gap-4 rounded-lg">
+		<div class="rounded-lg border p-4">stuff</div>
+		<div class="rounded-lg border p-4">
+			<h1>Welcome to SvelteKit</h1>
+			<p>
+				Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+			</p>
+		</div>
+	</div>
+</div>
