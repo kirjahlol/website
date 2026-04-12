0abd71d90226b13049e3c40aceee40b00844dc12
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-12 13:45:55 -0400

    feat: setup system theme support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Navbar from '../components/Navbar.svelte';
+
+	if (browser) {
+		const isThemeAuto = localStorage.getItem('theme-auto');
+		if (isThemeAuto === null) {
+			const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+			const defaultTheme = prefersDark ? 'mocha' : 'latte';
+			localStorage.setItem('theme', defaultTheme);
+			localStorage.setItem('theme-auto', 'true');
+			document.documentElement.className = defaultTheme;
+		}
+
+		const theme = localStorage.getItem('theme');
+		if (theme) {
+			document.documentElement.className = theme;
+		}
+	}
 
 	let { children } = $props();
 </script>
