0b29971d536210d5f1518a0dcc595b5d2d383a17
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-12 22:42:31 -0400

    feat: make error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center">
+	<h1 class="text-5xl">{page.status}</h1>
+	<p class="text-ctp-subtext0">{page.error?.message}</p>
+</div>
