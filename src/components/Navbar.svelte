cc4a5c86d58f30681866a91b4a5bdcdd1ea8ed58
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-13 14:05:21 -0400

    feat: add icons to navbar links and restyle some things

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import NavbarLink from './NavbarLink.svelte';
+	import { IconHome, IconQuestionMark } from '@tabler/icons-svelte';
 </script>
 
 <nav class="fixed bottom-0 z-50 w-screen border-t border-ctp-surface0 bg-ctp-mantle p-2">
 	<li class="flex justify-center gap-2">
-		<NavbarLink href="/" name="home" />
-		<NavbarLink href="/foo" name="foo" />
+		<NavbarLink href="/" LinkIcon={IconHome} name="Home" />
+		<NavbarLink href="/foo" LinkIcon={IconQuestionMark} name="Foo" />
 	</li>
 </nav>
--- src/components/NavbarLink.svelte
+++ src/components/NavbarLink.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { type ResolvedPathname } from '$app/types';
+	import { type Icon } from '@tabler/icons-svelte';
 
 	interface Props {
 		href: ResolvedPathname;
+		LinkIcon: Icon;
 		name: string;
 	}
 
-	let { href, name }: Props = $props();
+	let { href, LinkIcon, name }: Props = $props();
 </script>
 
 <!-- eslint-disable svelte/no-navigation-without-resolve -->
 <a
 	{href}
 	class={[
-		'rounded-lg px-4 py-2',
-		page.url.pathname === href ? 'bg-ctp-green-400 text-ctp-mantle' : ''
-	]}>{name}</a
+		'flex gap-2 rounded-lg px-4 py-2',
+		page.url.pathname === href ? 'bg-ctp-green-400 font-semibold text-ctp-mantle' : ''
+	]}><LinkIcon /> {name}</a
 >
