b01cb2d436e5d5934022da4ead2b599df0bdaea6
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-13 23:06:44 -0400

    feat: add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,8 +17,13 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {#if visible}
 	<div transition:fade={{ duration: 300 }}>
-		<Navbar />
-		<ThemeSwitcher />
-		{@render children()}
+		<div class="flex min-h-screen flex-col items-center justify-center gap-2">
+			<Navbar />
+			<ThemeSwitcher />
+			{@render children()}
+			<footer>
+				<a href="https://github.com/kirjahlol/website" target="_blank">Source code</a>
+			</footer>
+		</div>
 	</div>
 {/if}
