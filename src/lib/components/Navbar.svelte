1ce779fc4b0fc5a789f2eecb5e4527305b2065d1
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-21 20:03:16 -0400

    feat: create contact page

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import NavbarLink, { type LinkProps } from './NavbarLink.svelte';
-	import { IconHome } from '@tabler/icons-svelte';
+	import { IconHome, IconMail } from '@tabler/icons-svelte';
 
 	const links: LinkProps[] = [
 		{
@@ -8,6 +8,12 @@
 			href: '/',
 			LinkIcon: IconHome,
 			name: 'Home'
+		},
+		{
+			id: 2,
+			href: '/contact',
+			LinkIcon: IconMail,
+			name: 'Contact'
 		}
 	];
 </script>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,8 @@
+<section>
+	<h3>Methods</h3>
+	<p>
+		The best way to get in contact with me is through Discord (username is <code>kirjah</code>) or
+		Twitter. You <i>could</i> <a href="mailto:kirjah.lol@gmail.com" target="_blank">email me</a>,
+		but it is unlikely I will respond in a timely manner.
+	</p>
+</section>
