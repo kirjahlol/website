28631167350f2f99257d67ccc53ac1f6103f6424
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-21 20:39:02 -0400

    feat: change status styling

--- src/lib/components/Status.svelte
+++ src/lib/components/Status.svelte
@@ -31,7 +31,7 @@
 		{#if status}
 			<span class="mb-2 text-sm text-ctp-subtext0 italic">({status.timeAgo})</span>
 			<code class="mb-2 bg-ctp-base! p-4!"
-				><span class="text-ctp-green-400">> echo</span>
+				><span class="text-ctp-green-400">> <span class="font-semibold">echo</span></span>
 				<span class="text-ctp-yellow-400">"{status.face} {status.content}"</span><span
 					class="animate-blink text-ctp-rosewater-400">_</span
 				></code
