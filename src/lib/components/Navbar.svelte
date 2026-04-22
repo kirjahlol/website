0cb30f1bc4bcc6b95f8ccbe323916bc66a34c182
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-21 20:31:53 -0400

    feat: change navbar link appearance

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
 	import NavbarLink, { type LinkProps } from './NavbarLink.svelte';
-	import { IconHome, IconMail } from '@tabler/icons-svelte';
 
 	const links: LinkProps[] = [
 		{
 			id: 1,
 			href: '/',
-			LinkIcon: IconHome,
-			name: 'Home'
+			name: '~/'
 		},
 		{
 			id: 2,
 			href: '/contact',
-			LinkIcon: IconMail,
-			name: 'Contact'
+			name: '~/contact'
 		}
 	];
 </script>
 
-<nav class="rounded-lg border border-ctp-surface0 bg-ctp-mantle p-2">
-	<li class="flex gap-2">
+<nav class="rounded-lg border border-ctp-surface0 bg-ctp-mantle p-4">
+	<li class="flex gap-4">
 		{#each links as link (link.id)}
 			<NavbarLink {...link} />
 		{/each}
--- src/lib/components/NavbarLink.svelte
+++ src/lib/components/NavbarLink.svelte
@@ -1,36 +1,29 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import type { ResolvedPathname } from '$app/types';
-	import type { Icon } from '@tabler/icons-svelte';
 
 	export interface LinkProps {
 		id: number;
 		href: ResolvedPathname;
-		LinkIcon: Icon;
 		name: string;
 	}
 
-	let { href, LinkIcon, name }: LinkProps = $props();
+	let { href, name }: LinkProps = $props();
 
 	let active = $derived(page.url.pathname === href);
 </script>
 
 <!-- eslint-disable svelte/no-navigation-without-resolve -->
-<a
-	{href}
-	data-sveltekit-noscroll
-	class={['flex gap-2 rounded-md p-2 transition-[scale] duration-150', { active }]}
-	><LinkIcon /> {name}</a
->
+<a {href} data-sveltekit-noscroll class={[' rounded-md font-mono', { active }]}>{name}</a>
 
 <style>
 	@reference '@routes/layout.css';
 
 	a.active {
-		@apply pointer-events-none animate-spring bg-ctp-green-400 font-semibold text-ctp-mantle;
+		@apply pointer-events-none font-semibold text-ctp-green;
 	}
 
 	a:not(.active) {
-		@apply hover:bg-ctp-surface0/50 active:scale-90;
+		@apply hover:underline;
 	}
 </style>
