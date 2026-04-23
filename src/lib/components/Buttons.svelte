b8c0665a96a76f8efac5d4c0b88e0b02f6b191ac
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-22 20:05:10 -0400

    feat: add buttons to buttons component

---
+++ src/lib/components/Buttons.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	interface Button {
+		href?: string;
+		src: string;
+		alt: string;
+	}
+
+	const buttons: Button[] = [
+		{
+			href: 'https://trafficlunar.net/',
+			src: 'https://trafficlunar.net/88x31.gif',
+			alt: "Button for trafficlunar's website"
+		},
+		{
+			href: 'https://goooby.neocities.org/',
+			src: 'https://goooby.neocities.org/goooby88x31new.png',
+			alt: "Button for goob's website, Goooby"
+		},
+		{
+			href: 'https://petrapixel.neocities.org/',
+			src: 'https://cdn.jsdelivr.net/gh/petracoding/petrapixel.neocities.org@latest/public/assets/img/linkback.gif',
+			alt: "Button for Petra's website, petrapixel"
+		},
+		{
+			href: 'https://jeith.com/',
+			src: 'https://neocities.jeith.com/jeith.gif',
+			alt: "Button for jeith's website"
+		},
+		{
+			href: 'https://dimden.dev/',
+			src: 'https://dimden.dev/services/images/88x31.gif',
+			alt: "Button for dimden's site"
+		}
+	];
+</script>
+
+<section>
+	<h3>Buttons</h3>
+	<div class="flex flex-wrap justify-center">
+		{#each buttons as { href, src, alt }, i (i)}
+			{#if href}
+				<a {href} rel="external" target="_blank">
+					<img {src} {alt} class="h-7.75 w-22 [image-rendering:pixelated]" />
+				</a>
+			{:else}
+				<img {src} {alt} class="h-7.75 w-22 [image-rendering:pixelated]" />
+			{/if}
+		{/each}
+	</div>
+</section>
