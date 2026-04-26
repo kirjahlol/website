60a5377efc045267e4a25186e7c2e90ad9ef9ed5
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-26 10:20:30 -0400

    feat(components/Webrings): add WiiRing

--- src/components/global/Webrings.svelte
+++ src/components/global/Webrings.svelte
@@ -4,6 +4,7 @@
 	import Icon from '@iconify/svelte';
 </script>
 
+<!-- Catppuccin Webring -->
 <div class="flex items-center gap-2">
 	<a href="https://ctp-webr.ing/kirjah/previous">
 		<Icon icon="tabler:arrow-left" class="size-6" />
@@ -14,4 +15,13 @@
 	<a href="https://ctp-webr.ing/kirjah/next">
 		<Icon icon="tabler:arrow-right" class="size-6" />
 	</a>
+</div>
+
+<!-- WiiRing -->
+<div class="w-33.75">
+	<iframe
+		src="https://wiiring.jeith.com/snippet?site=https://kirjah.lol&variant=standard"
+		title="WiiRing"
+		class="w-full border-none"
+	></iframe>
 </div>
