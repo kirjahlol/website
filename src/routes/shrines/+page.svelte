e6200c3ce10da3098697befb3da277ddccb04d24
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-02 15:08:04 -0400

    feat(route/shrines): add basic definition

--- src/routes/shrines/+page.svelte
+++ src/routes/shrines/+page.svelte
@@ -1,1 +1,10 @@
-gorp
+<section>
+	<h3>What are Shrines?</h3>
+	<p>
+		<b>Shrines</b> are web pages that are dedicated to webmaster's interests, whether that is a specific
+		TV show, video game franchise, music artist, etc. Here, you can find some of the shrines I have made.
+		Check them out if you have the time!
+	</p>
+	<hr />
+	<p>shrines here</p>
+</section>
