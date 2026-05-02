4b938cbbd31ef3b9f8028c690e4908e8363be3cd
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-02 16:48:52 -0400

    fix: error page and formatting

---
+++ src/routes/+error.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+	import Header from '$components/global/Header.svelte';
+	import ThemeSwitcher from '$components/global/ThemeSwitcher.svelte';
+	import './(app)/layout.css';
+
+	if (browser) {
+		const isThemeSystem = localStorage.getItem('theme-system');
+		if (isThemeSystem === null) {
+			const isThemeDarkPreferred = window.matchMedia('(prefers-color-scheme: dark)').matches;
+			const defaultTheme = isThemeDarkPreferred ? 'mocha' : 'latte';
+			localStorage.setItem('theme', defaultTheme);
+			localStorage.setItem('theme-system', 'true');
+		}
+
+		const theme = localStorage.getItem('theme');
+		if (theme) {
+			document.documentElement.className = theme;
+		}
+	}
+</script>
+
+<div class="absolute flex w-screen justify-between p-4">
+	<Header />
+	<ThemeSwitcher />
+</div>
+<div class="flex h-screen flex-col items-center justify-center p-4">
+	{#if page.status === 404}
+		<h1 class="font-mono text-7xl font-semibold">404</h1>
+		<p class="mb-2 text-center text-xl/8 font-medium">Page not found. Sorry!</p>
+	{:else}
+		<h1 class="font-mono text-7xl font-semibold">{page.status}</h1>
+		<p class="mb-2 text-xl/8 font-medium">{page.error?.message}</p>
+	{/if}
+	<a href={resolve('/')} class="mb-2 rounded-lg bg-ctp-green-400 px-4 py-2 text-ctp-base"
+		>Go back home</a
+	>
+</div>
