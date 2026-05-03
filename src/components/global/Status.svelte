93ea99c279802f57b56d371693f5199a9b6217cc
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-03 11:10:48 -0400

    feat(components/`Status`): handle unloaded data and errors better

--- src/components/global/Status.svelte
+++ src/components/global/Status.svelte
@@ -5,7 +5,7 @@
 		timeAgo: string;
 	}
 
-	async function getStatusData<T>(): Promise<T> {
+	async function getStatus(): Promise<Status> {
 		const url = 'https://status.cafe/users/kirjahri/status.json';
 
 		const response = await fetch(url);
@@ -13,24 +13,28 @@
 			throw new Error(`Fetch failed: ${response.status}`);
 		}
 
-		return (await response.json()) as T;
+		const status = await response.json();
+		return status;
 	}
 
-	async function getStatus(): Promise<Status> {
-		const data = await getStatusData<Status>();
-		return data;
-	}
-
-	let status: Status | undefined = $state();
-	$effect(() => {
-		getStatus().then((value) => (status = value));
-	});
+	let statusPromise = getStatus();
 </script>
 
 <section>
 	<h3>Status</h3>
 	<div class="flex flex-col items-center">
-		{#if status}
+		{#await statusPromise}
+			<div class="mb-2 flex items-center gap-2">
+				<span>❓</span>
+				<span class="text-sm text-ctp-subtext0">(Loading...)</span>
+			</div>
+			<code class="mb-2 bg-ctp-base! p-4!"
+				><span class="text-ctp-green-400">> <span class="font-semibold">echo</span></span>
+				<span class="text-ctp-yellow-400">"Loading..."</span><span
+					class="animate-blink text-ctp-rosewater-400">_</span
+				></code
+			>
+		{:then status}
 			<div class="mb-2 flex items-center gap-2">
 				<span>{status.face}</span>
 				<span class="text-sm text-ctp-subtext0">({status.timeAgo})</span>
@@ -42,9 +46,15 @@
 					class="animate-blink text-ctp-rosewater-400">_</span
 				></code
 			>
-		{:else}
-			<p class="mb-2">Loading...</p>
-		{/if}
+		{:catch error}
+			<div class="mb-2 flex items-center gap-2">
+				<span>❌</span>
+				<span class="text-sm text-ctp-subtext0">(Error)</span>
+			</div>
+			<code class="mb-2 bg-ctp-base! p-4!"
+				><span class="font-semibold text-ctp-red-400">{error.message}</span></code
+			>
+		{/await}
 	</div>
 	<div class="flex justify-between text-xs text-ctp-subtext0">
 		<span>Powered by <a href="https://status.cafe/" target="_blank">Status Cafe</a></span>
