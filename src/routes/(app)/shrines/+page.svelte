91f8a37b4e5f282f8c0c1c415ce395c80b2648a3
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-03 09:55:50 -0400

    feat(`/shrines`): rename header and fix grammar

--- src/routes/(app)/shrines/+page.svelte
+++ src/routes/(app)/shrines/+page.svelte
@@ -14,9 +14,9 @@
 </script>
 
 <section>
-	<h3>What are Shrines?</h3>
+	<h3>About Shrines</h3>
 	<p>
-		<b>Shrines</b> are web pages that are dedicated to webmaster's interests, whether that is a specific
+		<b>Shrines</b> are web pages that are dedicated to the webmaster's interests, whether that is a specific
 		TV show, video game franchise, music artist, etc. Here, you can find some of the shrines I have made.
 		Check them out if you have the time!
 	</p>
