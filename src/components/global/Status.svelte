7a2f6d5c5c23dd65c78cca94f704e182f3e0d114
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-05 19:27:01 -0400

    refactor(components/`Status`): DRY

--- src/components/global/Status.svelte
+++ src/components/global/Status.svelte
@@ -27,13 +27,13 @@
 
 <section>
 	<h3>Status</h3>
-	<div class="flex flex-col items-center">
+	<div class="mb-2 flex flex-col items-center">
 		{#await statusPromise}
 			<div class="mb-2 flex items-center gap-2">
 				<span>❓</span>
 				<span class="text-sm text-ctp-subtext0">(Loading...)</span>
 			</div>
-			<code class="mb-2 bg-ctp-base! p-4!"
+			<code class="bg-ctp-base! p-4!"
 				><span class="text-ctp-green-400">> <span class="font-semibold">echo</span></span>
 				<span class="text-ctp-yellow-400">"Loading..."</span><span
 					class="animate-blink text-ctp-rosewater-400">_</span
@@ -45,7 +45,7 @@
 					<span>{status.face}</span>
 					<span class="text-sm text-ctp-subtext0">({status.timeAgo})</span>
 				</div>
-				<code class="mb-2 bg-ctp-base! p-4!"
+				<code class="bg-ctp-base! p-4!"
 					><span class="text-ctp-green-400">> <span class="font-semibold">echo</span></span>
 					<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 					<span class="text-ctp-yellow-400">"{@html status.content}"</span><span
@@ -58,7 +58,7 @@
 				<span>❌</span>
 				<span class="text-sm text-ctp-subtext0">(Error)</span>
 			</div>
-			<code class="mb-2 bg-ctp-base! p-4!"
+			<code class="bg-ctp-base! p-4!"
 				><span class="font-semibold text-ctp-red-400">{error.message}</span></code
 			>
 		{/await}
