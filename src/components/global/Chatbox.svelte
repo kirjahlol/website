d07b10397b765df6199482c7f42998bc76bf648b
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-05 19:25:23 -0400

    feat(components/`Chatbox`): add Cbox

--- src/components/global/Chatbox.svelte
+++ src/components/global/Chatbox.svelte
@@ -1,3 +1,17 @@
+<!-- Rare `<script>`-tag-less component -->
 <section>
 	<h3>Chatbox</h3>
+	<iframe
+		src="https://www3.cbox.ws/box/?boxid=3554935&boxtag=J86zdW"
+		width="100%"
+		height="200"
+		allowtransparency={true}
+		allow="autoplay"
+		frameborder="0"
+		marginheight="0"
+		marginwidth="0"
+		scrolling="auto"
+		title="Cbox"
+		class="mb-2 rounded-md border border-ctp-surface0"
+	></iframe>
 </section>
