5aedd981119e289b4a345f75abc5c0a986bdca76
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-06 21:25:37 -0400

    feat(components/`Socials`): add hover animation to buttons

--- src/components/global/Socials.svelte
+++ src/components/global/Socials.svelte
@@ -69,7 +69,7 @@
 				{href}
 				rel="external"
 				target="_blank"
-				class="rounded-md border border-ctp-surface0 bg-ctp-surface0/50 p-2 text-ctp-text!"
+				class="rounded-md border border-ctp-surface0 bg-ctp-surface0/50 p-2 text-ctp-text! transition-[scale] duration-150 hover:scale-110"
 				{@attach tooltip(name, 'bottom')}
 			>
 				<Icon {icon} class="size-6" />
