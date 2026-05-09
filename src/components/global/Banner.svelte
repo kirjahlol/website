f731b0677286a9dbce0554702b7bddcf213362a4
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-09 15:55:30 -0400

    feat(components): add splash texts to banner

--- src/components/global/Banner.svelte
+++ src/components/global/Banner.svelte
@@ -3,6 +3,7 @@
 	import { page } from '$app/state';
 	import banner1 from '$assets/banners/banner-1.webp';
 	import banner2 from '$assets/banners/banner-2.webp';
+	import SplashText from './SplashText.svelte';
 	import { onMount } from 'svelte';
 
 	interface Banner {
@@ -37,20 +38,29 @@
 	}
 </script>
 
-<div class="relative">
-	<a href={resolve('/')}>
-		<img src={banner.imageSource} alt="Banner" class="rounded-lg" />
-	</a>
-	<h1
-		class="absolute top-0 left-0 rounded-br-lg bg-ctp-base/75 p-1 font-mono text-sm font-semibold lg:p-2 lg:text-lg"
-	>
-		{hostname}
-	</h1>
-	<h2
-		class="absolute right-0 bottom-0 rounded-tl-lg bg-ctp-base/75 p-1 text-xs font-semibold lg:p-2 lg:text-base"
-	>
-		Credit: <a href={banner.creditUrl} rel="external" target="_blank" class="hover:underline"
-			>{banner.credit}</a
+<div class="rounded-lg border border-ctp-surface0 bg-ctp-mantle">
+	<div class="relative">
+		<a href={resolve('/')}>
+			<img
+				src={banner.imageSource}
+				alt="Banner"
+				class="rounded-t-lg border-b border-ctp-surface0"
+			/>
+		</a>
+		<h1
+			class="absolute top-0 left-0 rounded-tl-lg rounded-br-lg bg-ctp-base/75 p-1 font-mono text-sm font-semibold lg:p-2 lg:text-lg"
 		>
-	</h2>
+			{hostname}
+		</h1>
+		<h2
+			class="absolute right-0 bottom-0 mb-px rounded-tl-lg bg-ctp-base/75 p-1 text-xs font-semibold lg:p-2 lg:text-base"
+		>
+			Credit: <a href={banner.creditUrl} rel="external" target="_blank" class="hover:underline"
+				>{banner.credit}</a
+			>
+		</h2>
+	</div>
+	<div class="p-2">
+		<SplashText />
+	</div>
 </div>
---
+++ src/components/global/SplashText.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const splashTexts: string[] = [
+		"You'll never guess what my favorite Pokémon is!",
+		"Check out the Shrines page (or don't, I guess)!",
+		'gurt: yo',
+		'asdfjkl;',
+		'wow coming up with these is harder than i thought'
+	];
+
+	let splashText = $state(splashTexts[0]);
+	onMount(() => {
+		const randomSplashTextIndex = Math.floor(Math.random() * splashTexts.length);
+		splashText = splashTexts[randomSplashTextIndex];
+	});
+</script>
+
+<span class="flex items-center gap-2">
+	<h1 class="text-lg text-ctp-subtext1">
+		&ldquo;{splashText}&rdquo;
+	</h1>
+	<h2 class="text-sm text-ctp-subtext0">
+		({splashTexts.indexOf(splashText) + 1}/{splashTexts.length})
+	</h2>
+</span>
