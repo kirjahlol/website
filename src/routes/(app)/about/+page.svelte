23c57e96e76c0d980e21a805132b7861309a3f4e
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-05-10 11:45:28 -0400

    feat(`/about`): add bio

--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import ProfileCard, { type ProfileCardItem } from '$components/global/ProfileCard.svelte';
+	import ProfileCard, { type ProfileCardItem } from '$components/about/ProfileCard.svelte';
 	import { age, pronouns } from '$lib';
 
 	let profileCardItems: ProfileCardItem[] = [
@@ -23,5 +23,26 @@
 </script>
 
 <section>
-	<ProfileCard items={profileCardItems} />
+	<div class="flex flex-col items-center gap-2 sm:flex-row">
+		<ProfileCard items={profileCardItems} />
+		<div>
+			<h3>Bio</h3>
+			<p>
+				Hi! My name is <span class="font-semibold text-ctp-green-400">kirjah</span>. I'm a
+				self-taught programmer.
+			</p>
+			<p>
+				Some of my hobbies are coding things, listening to and (sometimes) producing music, drawing
+				digitally (you can find my art on Twitter), and playing in the
+				<a href="https://lounge.mkcentral.com/mkworld" target="_blank"
+					>competitive Mario Kart World Lounge</a
+				>
+				(you can find my stats
+				<a
+					href="https://lounge.mkcentral.com/mkworld/PlayerDetails/79021?season=2&p=12"
+					target="_blank">here</a
+				>).
+			</p>
+		</div>
+	</div>
 </section>
